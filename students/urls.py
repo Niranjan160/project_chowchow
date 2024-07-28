50428f9a0811e09587ecbf2e23709e13aa0d9e58
madhavan347 <madhavanshri.ms347@gmail.com>
2024-07-28 22:36:44 +0530

    added admin page list views

--- students/urls.py
+++ students/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.student_dashboard, name="student_dashboard"),
-    path("officeadmin/", views.admin_dashboard, name="admin_dashboard"),
     path("login/", views.loginPage, name="login_page"),
     path("logout/", views.logoutPage, name="logout_page"),
+    path("officeadmin/", views.admin_dashboard, name="admin_dashboard"),
+    path("officeadmin/studentdetails", views.admin_dashboard_student_details, name="admin_dashboard_student_details"),
+    path("officeadmin/pendingdetails", views.pending_receipt_details, name="admin_dashboard_pending_details"),
 ]
--- students/views.py
+++ students/views.py
@@ -1,33 +1,11 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
+from django.shortcuts import render,redirect
+from django.http import HttpResponseBadRequest
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from students.models import Student, Fees_detail
 from students.forms import fees_details_form
-
-@login_required
-def student_dashboard(request):
-    if request.user.is_office_admin(): 
-        return redirect("admin_dashboard")
-    context = {}
-    context["form"] = fees_details_form()
-    context["student"] = Student.objects.get(Student=request.user)
-    context["fees_history"] = Fees_detail.objects.filter(student_id=context["student"])
-    if request.method == "POST":
-        fees_details = fees_details_form(request.POST, request.FILES)
-        # print(fees_details.data)
-        if fees_details.is_valid():
-            fees_details = fees_details.save(commit=False)
-            fees_details.student_id = context["student"]
-            fees_details.save()
-    return render(request, 'students/student_dashboard.html', context=context)
-
-@login_required
-def admin_dashboard(request):
-    if request.user.is_student():
-       return redirect("student_dashboard")
-    return render(request, 'students/admin_dashboard.html')
-
+from datetime import date
+from django.core.paginator import Paginator
 
 def loginPage(request):
     if request.user.is_authenticated:
@@ -70,3 +48,66 @@ def logoutPage(request):
 #             return redirect('login')
 
 #         return render(request, 'appone/register.html', context)
+
+@login_required
+def student_dashboard(request):
+    if request.user.is_office_admin(): 
+        return redirect("admin_dashboard")
+    context = {}
+    context["form"] = fees_details_form()
+    context["student"] = Student.objects.get(Student=request.user)
+    context["fees_history"] = Fees_detail.objects.filter(student_id=context["student"])
+    if request.method == "POST":
+        fees_details = fees_details_form(request.POST, request.FILES)
+        if fees_details.is_valid():
+            fees_details = fees_details.save(commit=False)
+            fees_details.student_id = context["student"]
+            fees_details.save()
+    return render(request, 'students/student_dashboard.html', context=context)
+
+
+@login_required
+def admin_dashboard(request):
+    if request.user.is_student():
+       return redirect("student_dashboard")
+    context = {}
+    context["students_count"] = Student.objects.filter(
+        academic_end_year__year__gte=date.today().year
+    ).count()
+    return render(request, 'students/admin_dashboard.html', context)
+
+@login_required
+def pending_receipt_details(request):
+    context = {}
+    pending_fees_list = Fees_detail.objects.filter(receipt_status="PEN").order_by("-receipt_submitted_date")
+    paginator = Paginator(pending_fees_list, 20)
+    page_number = request.GET.get("page") or 1
+    context["page_obj"] = paginator.get_page(page_number)
+    return render(request, "students/admin_pending_details.html", context)
+
+@login_required
+def update_fees_receipt_status(request):
+    # Need to add exception
+    if request.method == "POST":
+        status = request.POST["status"]
+        reference_id = request.POST["reference_id"]
+        fees_detail = Fees_detail.objects.get(reference_id=reference_id)
+        fees_detail.receipt_status = status
+        fees_detail.save()
+        return redirect('admin_dashboard_student_details')
+    else: 
+        return HttpResponseBadRequest("Not like this :)")
+
+@login_required
+def admin_dashboard_student_details(request):
+    if request.user.is_student():
+       return redirect("student_dashboard")
+    context = {}
+    students_details = Student.objects.filter(
+        academic_end_year__year__gte=date.today().year
+    ).order_by('register_no')
+    paginator = Paginator(students_details, 30)
+    page_number = request.GET.get("page") or 1
+    context["page_obj"] = paginator.get_page(page_number)
+    return render(request, 'students/admin_dashboard_student_details.html', context)
+
